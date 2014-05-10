88889eb414eb3970d93773083f6632f0b1aa72c0
MrSaints <ian@fyianlai.com>
2014-05-10 03:55:58 +0100

    Added error message (via alert due to problems with Chrome notifications); it is triggered when auto renewal fails

--- assets/js/bex_auto.js
+++ assets/js/bex_auto.js
@@ -1,20 +1,17 @@
 'use strict';
 
-(function ($) {
-	var Renew = function (data) {
-		if (data.error) {
-			// Do something
-			return;
-		}
-
-		// Success
-		console.log("Renewed!");
+var Renew = function (data) {
+	if (data.error) {
+		return alert("We were unable to renew your loans due to a network error. Please try renewing your loans manually.");
 	}
+	for (var i = 0, length = data.items.length; i < length; i++) {
+		if (!data.items[i].wasRenewed)
+			return alert("We were unable to renew all your loans (there may be a request placed on one of your loans). Please try renewing your loans manually.");
+	}
+	console.log("All library loans were successfully renewed.")
+}
 
-	chrome.storage.sync.get("autoRenew", function (data) {
-		if (data.autoRenew === true)
-			$.getJSON('https://moodle.lse.ac.uk/blocks/lswt/renew.php', Renew);
-		else
-			console.log("Disabled");
-	});
-}(jQuery));
+chrome.storage.sync.get("autoRenew", function (data) {
+	if (data.autoRenew !== true) return;
+	$.getJSON('https://moodle.lse.ac.uk/blocks/lswt/renew.php', Renew);
+});
