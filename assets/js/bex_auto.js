1cfd37b88359e9f51fbfde026c770514ffbd30aa
MrSaints <ian@fyianlai.com>
2014-05-10 04:17:16 +0100

    Removed jQuery as a dependency; and hidden search feature (untill it is fixed)

--- assets/js/bex_auto.js
+++ assets/js/bex_auto.js
@@ -2,7 +2,7 @@
 
 var Renew = function (data) {
 	if (data.error) {
-		return alert("We were unable to renew your loans due to a network error. Please try renewing your loans manually.");
+		return alert("We were unable to renew your loans due to an unknown error. Please try renewing your loans manually.");
 	}
 	for (var i = 0, length = data.items.length; i < length; i++) {
 		if (!data.items[i].wasRenewed)
@@ -11,7 +11,19 @@ var Renew = function (data) {
 	console.log("All library loans were successfully renewed.")
 }
 
+var RenewLoans = function (URL, callback) {
+	var request = new XMLHttpRequest();
+	request.open('GET', URL, true);
+	request.onload = function() {
+		callback(JSON.parse(request.responseText));
+	}
+	request.onerror = function() {
+		return alert("We were unable to renew your loans due to a network error. Please try renewing your loans manually.");
+	}
+	request.send();
+}
+
 chrome.storage.sync.get("autoRenew", function (data) {
 	if (data.autoRenew !== true) return;
-	$.getJSON('https://moodle.lse.ac.uk/blocks/lswt/renew.php', Renew);
+	RenewLoans('https://moodle.lse.ac.uk/blocks/lswt/renew.php', Renew);
 });
